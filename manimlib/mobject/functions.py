0595d3fa02e26f2018858729b95f949185c972a5
SidewayOutput <77378619+SidewayOutput@users.noreply.github.com>
2021-03-06 18:14:12 -0800

    Update features (manim-01Mar21)

--- manimlib/mobject/functions.py
+++ manimlib/mobject/functions.py
@@ -1,29 +1,79 @@
-from manimlib.constants import *
-from manimlib.mobject.types.vectorized_mobject import VMobject
+from numpy import arange, array
+from manimlib.constants import YELLOW
+from manimlib.mobject.types.vectorized_mobject import VGroup, VMobject
 from manimlib.utils.config_ops import digest_config
-import math
 
 
-class ParametricFunction(VMobject):
+class ParametricCurve(VMobject):
+    CONFIG = {
+        "t_range": [0, 1, 0.1],
+        "epsilon": 1e-8,
+        # TODO, automatically figure out discontinuities
+        "discontinuities": [],
+        "use_smoothing": True,
+    }
+
+    def __init__(self, t_func, t_range=None, **kwargs):
+        digest_config(self, kwargs)
+        if t_range is not None:
+            self.t_range[:len(t_range)] = t_range
+        # To be backward compatible with all the scenes specifying t_min, t_max, step_size
+        self.t_range = [
+            kwargs.get("t_min", self.t_range[0]),
+            kwargs.get("t_max", self.t_range[1]),
+            kwargs.get("step_size", self.t_range[2]),
+        ]
+        self.t_func = t_func
+        VMobject.__init__(self, **kwargs)
+
+    def get_point_from_function(self, t):
+        return self.t_func(t)
+
+    def init_points(self):
+        t_min, t_max, step = self.t_range
+
+        jumps = array(self.discontinuities)
+        jumps = jumps[(jumps >= t_min) & (jumps <= t_max)]
+        if len(jumps) > 0:
+            if t_min == jumps[0]:
+                t_min = jumps[0]+self.epsilon
+                jumps = jumps[1:]
+            if t_max == jumps[-1]:
+                t_max = jumps[0]-self.epsilon
+                jumps = jumps[:-1]
+        boundary_times = [t_min, t_max, *
+                          (jumps - self.epsilon), *(jumps + self.epsilon)]
+        boundary_times.sort()
+        for t1, t2 in zip(boundary_times[0::2], boundary_times[1::2]):
+            #t_range = [*arange(t1, t2, step), t2]
+            t_range = list(arange(t1, t2, step))
+            if t_range[-1] != t2:
+                t_range.append(t2)
+
+            points = array([self.t_func(t) for t in t_range])
+            self.start_new_path(points[0])
+            self.add_points_as_corners(points[1:])
+        if self.use_smoothing:
+            self.make_smooth()
+            # self.make_approximately_smooth()
+
+        return self
+
+
+class ParametricFunction(ParametricCurve):
     CONFIG = {
         "t_min": 0,
         "t_max": 1,
         "step_size": 0.01,  # Use "auto" (lowercase) for automatic step size
-        "dt": 1e-8,
-        # TODO, be smarter about figuring these out?
-        "discontinuities": [],
     }
 
-    def __init__(self, function=None, **kwargs):
-        # either get a function from __init__ or from CONFIG
-        self.function = function or self.function
-        VMobject.__init__(self, **kwargs)
+    def __init__(self, t_func, **kwargs):
+        digest_config(self, kwargs)
+        self.t_range = [self.t_min, self.t_max, self.get_step_size()]
+        ParametricCurve.__init__(self, t_func, **kwargs)
 
     def get_function(self):
         return self.function
-
-    def get_point_from_function(self, t):
-        return self.function(t)
 
     def get_step_size(self, t=None):
         if self.step_size == "auto":
@@ -46,59 +96,49 @@ class ParametricFunction(VMobject):
         else:
             return self.step_size
 
-    def generate_points(self):
-        t_min, t_max = self.t_min, self.t_max
-        dt = self.dt
 
-        discontinuities = filter(
-            lambda t: t_min <= t <= t_max,
-            self.discontinuities
-        )
-        discontinuities = np.array(list(discontinuities))
-        boundary_times = [
-            self.t_min, self.t_max,
-            *(discontinuities - dt),
-            *(discontinuities + dt),
-        ]
-        boundary_times.sort()
-        for t1, t2 in zip(boundary_times[0::2], boundary_times[1::2]):
-            t_range = list(np.arange(t1, t2, self.get_step_size(t1)))
-            if t_range[-1] != t2:
-                t_range.append(t2)
-            points = np.array([self.function(t) for t in t_range])
-            valid_indices = np.apply_along_axis(
-                np.all, 1, np.isfinite(points)
-            )
-            points = points[valid_indices]
-            if len(points) > 0:
-                self.start_new_path(points[0])
-                self.add_points_as_corners(points[1:])
-        self.make_smooth()
-        return self
-
-
-class FunctionGraph(ParametricFunction):
+class FunctionGraph(ParametricCurve):
     CONFIG = {
         "color": YELLOW,
-        "x_min": -FRAME_X_RADIUS,
-        "x_max": FRAME_X_RADIUS,
+        "x_range": [-8, 8, 0.01],
+        # "x_min": -FRAME_X_RADIUS,
+        # "x_max": FRAME_X_RADIUS,
+        # "step_size": 0.01,
     }
 
-    def __init__(self, function, **kwargs):
+    def __init__(self, function, x_range=None, **kwargs):
         digest_config(self, kwargs)
-        self.parametric_function = \
-            lambda t: np.array([t, function(t), 0])
-        ParametricFunction.__init__(
-            self,
-            self.parametric_function,
-            t_min=self.x_min,
-            t_max=self.x_max,
+        self.function = function
+        if x_range is not None:
+            self.x_range[:len(x_range)] = x_range
+
+        def parametric_function(t):
+            return [t, function(t), 0]
+        super().__init__(
+            parametric_function,
+            # t_min=self.x_min,
+            # t_max=self.x_max,
+            self.x_range,
             **kwargs
         )
-        self.function = function
 
     def get_function(self):
         return self.function
 
     def get_point_from_function(self, x):
-        return self.parametric_function(x)
+        return self.t_func(x)
+        # return self.parametric_function(x)
+
+
+class FunctionsGraph(VGroup):  # append_vectorized_mobject
+    def __init__(self, *functiongraphs, **kwargs):
+        VGroup.__init__(self, **kwargs)
+        functionsgraphs = []
+        # self.add(exec("FunctionGraph("+functiongraphs[0]+")"))
+        [exec("functionsgraphs.append(FunctionGraph("+functiongraph+"))",
+              {"functionsgraphs": functionsgraphs, "FunctionGraph": FunctionGraph}) for functiongraph in functiongraphs]
+
+        vmobject = VMobject()
+        [vmobject.append_vectorized_mobject(
+            functionsgraph) for functionsgraph in functionsgraphs]
+        self.add(vmobject)
