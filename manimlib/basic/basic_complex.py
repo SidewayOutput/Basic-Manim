7db2f78fd6caf0c2cca8243e8c6b625dbbfa24be
SidewayOutput <77378619+SidewayOutput@users.noreply.github.com>
2022-01-25 00:01:09 -0500

    Update basic_complex.py

--- manimlib/basic/basic_complex.py
+++ manimlib/basic/basic_complex.py
@@ -1,19 +1,22 @@
 import numpy as np
-
-from manimlib.animation.animation import AGroup
-from manimlib.animation.creation import ShowSubmobjectsOneByOne
-from manimlib.animation.composition import AnimationGroup
+import  manimlib.constants as V
+from manimlib.animation.animation import Animation, AGroup
+from manimlib.animation.creation import ShowSubmobjectsOneByOne,Write,Show
+from manimlib.animation.composition import AnimationGroup,OneByOne,Succession
 from manimlib.animation.fading import FadeIn
-from manimlib.animation.growing import GrowFromCenter
-from manimlib.basic.basic_animation import FadeInThenIndicate, FadeoutSuccession, ShowPassingFlashAndIndicateThenFadeOut
+from manimlib.animation.growing import GrowFromCenter,DiminishToSide,DiminishToEdge
+from manimlib.animation.transform import Restore,ApplyMethod
+from manimlib.basic.basic_animation import FadeInThenIndicate, FadeoutSuccession, ShowPassingFlashAndIndicateThenFadeOut,Freeze,GrowTitle
 
 from manimlib.basic.basic_compound import ShowSubmobjectsOneByOneAndFadeInThenIndicateThenFadeOut
-from manimlib.basic.basic_mobject import ImageMobjectGroup, MobjectOrChars
+from manimlib.basic.basic_mobject import ImageMobjectGroup, MobjectOrChars,OrderedGroup,AnimatedGroup
+from manimlib.basic.basic_geometry import GeomPosition
 from manimlib.constants import DOWN, GREEN, PoweredBy, Project, UP, WHITE, YELLOW
 from manimlib.mobject.geometry import Circle, RegularPolygon, Square, Triangle
 from manimlib.mobject.mobject import Group, Mobject
 from manimlib.mobject.shape_matchers import Underline
 from manimlib.mobject.svg.tex_mobject import TextMobject
+from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.utils.rate_functions import linear
 
 
@@ -163,11 +166,10 @@ class StartScreens01(AnimationGroup):
                 animations.add(GrowFromCenter(Underline(mobjs_3[1])))
             if len(mobjs_3[2]) > 0:
                 animations.add(FadeInThenIndicate(
-                    mobjs_3[2], run_time=1, scale_factor=1, color=None))
+                    mobjs_3[2], run_time=0.5, scale_factor=1.2, color=None))
             startscreens.add(FadeoutSuccession(AnimationGroup(
                 *animations, run_time=mobjes_3_run_time), run_time=0.05))
         super().__init__(AnimationGroup(*startscreens, lag_ratio=1), **kwargs)
-
 
 class EndScreen01(AnimationGroup):
     def __init__(self, chars=r"\textrm{\textbf{THANKS\ FOR\ WATCHING}}", **kwargs):
@@ -177,7 +179,9 @@ class EndScreen01(AnimationGroup):
         )
 
 class PlayMobject(AnimationGroup):
-    def __init__(self, mobjs="", mobj=None, scale=1,shift=[0,0,0], **kwargs):
+    def __init__(self, mobjs="", mobj=Circle(), scale=1,shift=[0,0,0],offset=None,run_time=1, clear=True, **kwargs):
+        if offset is None:
+            offset=shift
         if mobjs != None:
             if mobjs == "":
                 try:
@@ -187,9 +191,107 @@ class PlayMobject(AnimationGroup):
                     mobjs = ImageMobjectGroup(np.char.mod(
                         '%01d', range(9, -1, -1)), "001\\")
         #if mobj!=None:
-        t=mobj.get_critical_point([-1,1,0])-mobjs.get_critical_point([-1,1,0])
-        mobjs.shift(t).shift(shift)
+        t=mobj.update().get_critical_point([-1,1,0])-mobjs.get_critical_point([-1,1,0])
+        [each.add_updater(lambda mob,mob0=mobj:mob.shift(mob0.get_critical_point([-1,1,0])-mob.get_critical_point([-1,1,0])).shift(offset)) for each in mobjs]
+        if clear:
+            mobjs.add(Mobject())
         super().__init__(
-            ShowSubmobjectsOneByOne(mobjs.add(
-                Mobject()).scale(scale),**kwargs),
+            ShowSubmobjectsOneByOne(mobjs.update().scale(scale),run_time=run_time,**kwargs),
         )
+
+class SlideShow01(AnimationGroup):
+    def __init__(self, paras,anims,sound,
+        i_start=1, i_count=None,i_first=None,
+        position=GeomPosition([0, 3.3, 0]),width=[6.75],height=7, 
+        mobjs=None, scene=None, animate=True,
+        **kwargs):
+        if i_count is None:
+            i_count=len(paras)
+        if i_first is None:
+            i_first=i_start
+        if scene is None:
+            scene=V.scene
+        if mobjs is None:
+            mobjs=AnimatedGroup().scale(0.2).post_to(scene)
+        animations=AGroup()
+        if 1==1 and paras[0] is not None:
+            animations.add(AnimationGroup(Animation(Mobject()),sound=sound[0],xaction="sound"))
+            animations.add(AnimationGroup(GrowTitle(paras[0]),  Freeze(4),anims[0],foreground=True,xaction="post"))
+        i_end = min((i_start+i_count), len(paras),  len(anims))
+        print(i_end,i_start+i_count,len(paras),len(paras))
+        group = VGroup(position)
+        OrderedGroup(group, ["/"]*(i_start-1) +
+                        paras[i_start:i_end],  width=width).post_to(scene)
+        block = VGroup()
+        print("i",anims[0].run_time)
+        
+        anim=Animation(Mobject())
+        for i in range(i_start, i_end):
+            print("i",i,anims[i].run_time)
+            group[i].add_to_group(block)
+            recycle = VGroup()
+            if animate:
+                anim=anims[i]
+            while block.get_height() > height:
+                if len(block) == 1:
+                    group[i].stretch_to_fit_height(height-0.05)
+                else:
+                    block[0].add_to_group(recycle).remove_from_group(block)
+            if i >= i_first:
+                animations.add(AnimationGroup(Animation(Mobject()),sound=sound[i], time_offset=0.5,xaction="sound"))
+                animations.add(AnimationGroup(anim,PlayMobject(mobjs.copy().next_to(group[i]), group[i]), DiminishToSide(recycle), Write(group[i]), ))
+            else:
+                animations.add(AnimationGroup(animDiminishToSide(recycle), Animation(group[i]), xaction="display"))#anim,
+        super().__init__(*animations)
+
+class SlideShow02(AnimationGroup):
+    def __init__(self,
+        parbs,anibs,sndbs,
+        j_start=1,j_first=None, j_count=None,
+        i_start=1,i_first=None,i_count=None,
+        j_position=GeomPosition([-6.5, 3.3, 0]),j_width=[13],j_height=7,
+        i_position=GeomPosition([0, 3.3, 0]),i_width=[6.75],i_height=7, 
+        mobjs=None, scene=None, **kwargs):
+        if j_count is None:
+            j_count=len(parbs[0])
+        if j_first is None:
+            j_first=j_start
+        if scene is None:
+            scene=V.scene
+        if mobjs is None:
+            mobjs=AnimatedGroup().scale(0.2).post_to(scene)
+        animations=AGroup()
+        if 1==1:
+            animations.add(AnimationGroup(Animation(Mobject()),sound=sndbs[0][0],xaction="sound"))
+            animations.add(AnimationGroup(GrowTitle(parbs[0][0]),  Freeze(4),anibs[0][0],foreground=True,xaction="post"))
+        j_end = min((j_start+j_count), len(parbs[0]),  len(anibs[0]))
+        grpa = VGroup(j_position)
+        OrderedGroup(grpa,  ["/"]*(j_start-1)+parbs[0][j_start:],  width=j_width).post_to(scene)#["/"]*(i_start-1)+
+        blka = VGroup().save_state()
+        blk=[grpa[j_first:j_first+i+1] for i in range(j_count+1)]
+        blk[0].save_state()
+        for j in range(j_start, j_end):
+            print("j",j)
+            grpa[j].add_to_group(blka)
+            rcca = VGroup()
+            while blka.get_height() > j_height:
+                if len(blka) == 1:
+                    grpa[j].stretch_to_fit_height(j_height-0.05)
+                else:
+                    blka[0].add_to_group(rcca).remove_from_group(blka)
+            if j >= j_first:
+                animations.add(AnimationGroup(Animation(Mobject()),sound=sndbs[0][j],xaction="sound"))
+                animations.add(AnimationGroup(PlayMobject(mobjs.copy().next_to(grpa[j]), grpa[j]), DiminishToSide(rcca), Write(grpa[j]), anibs[0][j], Freeze(2)))
+            else:
+                animations.add(AnimationGroup(DiminishToSide(rcca), Animation(grpa[j]), anibs[0][j],xaction="display"))
+            if 1 == 1 and j >= j_first:
+                animations.add(AnimationGroup(DiminishToEdge(blk[j-j_first].save_state()), run_time=0.5))
+                animations.add(*SlideShow01(parbs[j],anibs[j],sndbs[j],i_start, i_count,i_first).animations)
+                animations.add(AnimationGroup(exclude_mobjs="foreground", xaction="fadeout"))
+                if j<j_end-1:
+                    animations.add(AnimationGroup(Restore(blk[j-j_first]), run_time=0.5))
+
+        super().__init__(
+            *animations,
+        )
+
