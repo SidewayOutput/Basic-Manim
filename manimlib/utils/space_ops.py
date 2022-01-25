152a62f374a579f3ec49d69bc4d005f0fc80f00a
SidewayOutput <77378619+SidewayOutput@users.noreply.github.com>
2022-01-24 23:54:18 -0500

    Update space_ops.py

--- manimlib/utils/space_ops.py
+++ manimlib/utils/space_ops.py
@@ -32,6 +32,7 @@ def quaternion_mult(*quats):
             w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2,
         ]
     return result
+
 def zquaternion_mult(q1, q2):
     w1, x1, y1, z1 = q1
     w2, x2, y2, z2 = q2
@@ -47,6 +48,7 @@ def quaternion_from_angle_axis(angle, axis, axis_normalized=False):
     if not axis_normalized:
         axis = normalize(axis)
     return [math.cos(angle / 2), *(math.sin(angle / 2) * axis)]
+
 def zquaternion_from_angle_axis(angle, axis):
     return np.append(
         np.cos(angle / 2),
@@ -119,6 +121,7 @@ def rotation_matrix_transpose(angle, axis):
         ]
     quat = quaternion_from_angle_axis(angle, axis)
     return rotation_matrix_transpose_from_quaternion(quat)
+
 def rotation_matrix(angle, axis):
     """
     Rotation in R^3 about a specified axis of rotation.
@@ -130,11 +133,10 @@ def rotation_matrix(angle, axis):
 
 
 def rotation_about_z(angle):
-    return [
-        [np.cos(angle), -np.sin(angle), 0],
-        [np.sin(angle), np.cos(angle), 0],
-        [0, 0, 1]
-    ]
+    c, s= np.cos(angle), np.sin(angle)
+    return [[c, -s, 0],
+            [s,  c, 0],
+            [0,  0, 1]]
 
 
 def z_to_vector(vector):
@@ -189,6 +191,25 @@ def angle_between_vectors(v1, v2):
         np.dot(v1, v2),
         get_norm(v1) * get_norm(v2)
     ))
+
+def spaceangle_between(v1, v2, d=1):
+    [x1, y1, z1], [x2, y2, z2] = v1, v2
+    #[n1,n2,n3]=get_unit_normal(v1, v2)
+    if d:
+     [n1,n2,n3] = [0,0,1]
+    else:
+        [n1,n2,n3] = [0,0,-1]
+    radangle= np.arctan2(np.dot([n1,n2,n3],np.cross([x1,y1,z1],[x2,y2,z2])),np.dot([x1,y1,z1],[x2,y2,z2]))
+    if radangle<0:
+        radangle=TAU+radangle
+    return radangle
+    '''
+    [x1, y1, z1], [x2, y2, z2] = v1, v2
+    dot = np.dot(v1,v2)#x1*x2 + y1*y2 + z1*z2    #between [x1, y1, z1] and [x2, y2, z2]
+    lenSq1 = x1*x1 + y1*y1 + z1*z1
+    lenSq2 = x2*x2 + y2*y2 + z2*z2
+    return np.arccos(dot/np.sqrt(lenSq1 * lenSq2))
+    '''
 
 
 def project_along_vector(point, vector):
@@ -257,6 +278,10 @@ def line_intersection(line1, line2):
     each defined with a pair of vectors determining
     the end points
     """
+    if not isinstance(line1,(list,tuple)):
+        line1=line1.pts()
+    if not isinstance(line2,(list,tuple)):
+        line2=line2.pts()
     x_diff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     y_diff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
@@ -279,3 +304,52 @@ def get_winding_number(points):
         d_angle = ((d_angle + PI) % TAU) - PI
         total_angle += d_angle
     return total_angle / TAU
+
+
+# Linear interpolation variants from bezier.py
+
+def interpolate(start, end, alpha):
+    return (1 - alpha) * start + alpha * end
+    #(end-stat)*alpha+start
+
+def interslice(start, end, count, first=0,last=1):
+    '''(n)'''
+    return np.multiply(end-start,np.transpose([np.linspace(first, last,count)]))+start
+    #[interpolate(points[:-1], points[1:], a) for a in np.linspace(0, 1, nppcc)]
+    #np.multiply.outer(np.transpose([3,2,4]),np.linspace(3, 1,3)).transpose()
+    #np.multiply.outer(np.transpose(a),np.linspace(3, 1,3)).transpose()+a
+def integer_interpolate(start, end, alpha):
+    """
+    alpha is a float between 0 and 1.  This returns
+    an integer between start and end (inclusive) representing
+    appropriate interpolation between them, along with a
+    "residue" representing a new proportion between the
+    returned integer and the next one of the
+    list.
+
+    For example, if start=0, end=10, alpha=0.46, This
+    would return (4, 0.6).
+    """
+    if alpha >= 1:
+        return (end - 1, 1.0)
+    if alpha <= 0:
+        return (start, 0)
+    value = int(interpolate(start, end, alpha))
+    residue = ((end - start) * alpha) % 1
+    return (value, residue)
+
+
+def mid(start, end):
+    return (start + end) / 2.0
+
+
+def inverse_interpolate(start, end, value):
+    return np.true_divide(value - start, end - start)
+
+
+def match_interpolate(new_start, new_end, old_start, old_end, old_value):
+    return interpolate(
+        new_start, new_end,
+        inverse_interpolate(old_start, old_end, old_value)
+    )
+
