b9102bb5729caa473e2ba63c67658a2a6d6cecab
SidewayOutput <77378619+SidewayOutput@users.noreply.github.com>
2022-12-05 00:20:07 -0500

    Update rate_functions.py

--- manimlib/utils/rate_functions.py
+++ manimlib/utils/rate_functions.py
@@ -3,8 +3,14 @@ from manimlib.utils.bezier import bezier
 from manimlib.utils.simple_functions import sigmoid
 
 
-def linear(t):
-    return t
+def linear(t,k=0,a=0):
+    #1 - np.exp(-t / half_life)
+    if a==0:
+        return t+k
+    elif a>0:
+        return t**a+k
+    else:
+        return 1-abs(1-t)**(abs(a))+k
 
 
 def linear_with_pause(t, pause_timing=2./3, pause_duration=1.0/5, funx=linear, **kwargs):
@@ -15,16 +21,60 @@ def linear_with_pause(t, pause_timing=2./3, pause_duration=1.0/5, funx=linear, *
     ratio = 1/(1-pause_duration)
     pause_actual_timing = (1-pause_duration)*pause_timing
     if new_t <= pause_actual_timing:
+        #print(" y",pause_actual_timing, new_t,new_t*ratio)
         return new_t*ratio
     elif new_t > pause_actual_timing+pause_duration:
+        #print(" z",pause_actual_timing+pause_duration,new_t,(new_t-pause_duration)*ratio)
         return (new_t-pause_duration)*ratio
     else:
+        #print(" X",new_t,pause_actual_timing*ratio)
         return pause_actual_timing*ratio
 
 
 def pause(t, pause=2./3):
     return linear_with_pause(t, pause_timing=pause)
 
+
+def linear_with_pauses(t, pause_timing=[1./3,1.5/3,2./3], pause_duration=0.5/5, funx=linear, **kwargs):
+    '''
+    t, pause_timing=[1./3,1.5/3,2./3], pause_duration=0.5/5, func=linear,
+    '''
+    new_t =funx(t)
+    d=pause_duration
+    q=pause_timing    
+    if isinstance(q,(int,float)):
+        q=[q]
+    p=pause_actual_timing = (1-len(q)*d)*np.array(q)
+    t1=np.concatenate([[-0.1],list(np.array(p)+d),[1.1],[1.1],[1.1]])
+    t2=np.concatenate([p,[1.1],[1.1],[1.1],[1.1]])
+    c=np.concatenate(list(zip(t1,t2)))
+    ratio = 1/(1-len(q)*d)
+    for i in range(1,len(c),2):
+        x=int((i+1)/2-1)#123
+        if c[i-1]< new_t and (new_t) <= c[i]+x*d:
+
+            return (new_t-x*d)*ratio# new_t*ratio
+        elif c[i]+x*d< new_t and new_t <= c[i+1]+x*d:
+            return c[i]*ratio
+    '''
+    print(t1,t2)
+    for i in range(len(q)):
+        if t1[i]< new_t and (new_t) <= t2[i]+i*d:
+
+            return (new_t-i*d)*ratio# new_t*ratio
+        elif t2[i]+i*d< new_t and new_t <= t1[i+1]+i*d:
+            return t2[i]*ratio
+    '''
+
+
+def pauses(t, pause=[1./3,1.5/3,2./3], duration=0.5/5):
+    return linear_with_pauses(t, pause_timing=pause, pause_duration=duration)
+
+
+def pause_count(t, count=5, ratio=0.2,slice=slice(1,None)):
+    pause=np.linspace(0,1,count+1)[slice]
+    duration=1/count*ratio
+    return linear_with_pauses(t, pause_timing=pause, pause_duration=duration)
 
 def linear_with_delay(t, delay=5.0/10, **kwargs):
     '''
@@ -51,7 +101,7 @@ def wait(t, wait=5.0/10):
     return linear_with_wait(t, wait=wait)
 
 
-def linear_pulse(t, timing=1./5, transition=1.0/5, duration=1.0/5,transition2=1.0/5,stop=1, start=0.,end=0., **kwargs):
+def linear_pulse(t, timing=1./5, transition=1.0/5, duration=1.0/5,transition2=1.0/5,pulse=1,stop=1, start=0.,end=0., **kwargs):
     '''
     t, timing=2./3, transition=1.0/5,
     -
@@ -66,11 +116,12 @@ def linear_pulse(t, timing=1./5, transition=1.0/5, duration=1.0/5,transition2=1.
     if t <= t1 or t>t4 or t>t5:
         return start
     elif t >= t2 and t<=t3:
-        return end
+        return pulse
     elif t >t1 and t< t2:
-        return abs(start-(t-t1)/transition)
+        #return abs(start-(t-t1)/transition)
+        return (t-t1)/transition-start
     else:
-        return abs(end-(t-t3)/transition2)
+        return pulse-(t-t3)/transition2
         
 def pulsex(t, pulse_timing=2./3, pulse_duration=1.0/5, **kwargs):
     '''
@@ -151,6 +202,7 @@ def zshorten(t,shorten=1.0/5, **kwargs):
 def shorten(t,shorten=1.0/5, **kwargs):
     shortened=1-shorten
     return linear_with_pause(t/shortened, timing=shortened, transition=shorten, **kwargs)
+    #return linear_with_pause(t/shortened, pause_timing=1, pause_duration=0, **kwargs)
 
 
 def smooth(t, inflection=10.0):
